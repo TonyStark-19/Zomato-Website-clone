f0530626441f38b0f86dc281aaf76ea60ebb2b0e
Aditya chandel <adityachandel371@gmail.com>
2025-03-15 18:08:29 +0530

    Added option box functionality

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// js logic
+// js logic for checkbox
 
 document.addEventListener('DOMContentLoaded', function () {
     const emailCheck = document.querySelector('.email-check');
@@ -23,4 +23,20 @@ document.addEventListener('DOMContentLoaded', function () {
 
     // Initial check on page load
     toggleInputBox();
+});
+
+// js logic for explore options
+
+document.addEventListener('DOMContentLoaded', function () {
+    const optionBoxes = document.querySelectorAll(".option-box");
+
+    optionBoxes.forEach(optionBox => {
+        const downIcon = optionBox.querySelector("#down-icon");
+        const description = optionBox.querySelector(".option-description");
+
+        optionBox.addEventListener("click", () => {
+            description.classList.toggle("hidden");
+            downIcon.classList.toggle("active");
+        });
+    });
 });
