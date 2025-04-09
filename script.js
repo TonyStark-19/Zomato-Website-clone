9da87c90273155952a155434202824380494d4fd
Aditya chandel <adityachandel371@gmail.com>
2025-04-09 21:47:29 +0530

    Added menu bar logic for main page navbar

--- script.js
+++ script.js
@@ -40,3 +40,23 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     });
 });
+
+// JavaScript to toggle the menu
+
+document.addEventListener('DOMContentLoaded', function () {
+    const menu = document.querySelector('.menu');
+    const menuBtn = document.querySelector('.menu-btn');
+    const menuBtn2 = document.querySelector('.menu-btn-2');
+
+    menuBtn.addEventListener('click', function () {
+        menu.classList.toggle('active');
+        menuBtn.classList.add('hidden'); // Hide menu button
+        menuBtn2.classList.remove('hidden'); // Show close button
+    });
+
+    menuBtn2.addEventListener('click', function () {
+        menu.classList.toggle('active');
+        menuBtn2.classList.add('hidden'); // Hide close button
+        menuBtn.classList.remove('hidden'); // Show menu button
+    });
+});
